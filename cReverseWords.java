74137b59d6ef6b752ff125356ac97c90086f4565
arpansingh <arpansingh8583@gmail.com>
2026-05-11 14:37:38 +0530

    basic & 3 prob

---
+++ cReverseWords.java
@@ -0,0 +1,34 @@
+public class cReverseWords {
+
+    public static String reverseWords(String s) {
+
+        // Remove starting and ending spaces
+        s = s.trim();
+
+        // Split string by one or more spaces
+        String[] words = s.split("\\s+");
+
+        // StringBuilder for final answer
+        StringBuilder result = new StringBuilder();
+
+        // Traverse from last word to first
+        for (int i = words.length - 1; i >= 0; i--) {
+
+            result.append(words[i]);
+
+            // Add space except after last word
+            if (i != 0) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+
+        String s = "  i   love   java  ";
+
+        System.out.println(reverseWords(s));
+    }
+}
